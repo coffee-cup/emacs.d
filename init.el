e24614f0d3dd74127dbc3546f08c79a9d9a47238
Jake Runzer <jakerunzer@gmail.com>
2018-11-12 00:19:48 +0000

    doom-one theme and applications hydra

--- init.el
+++ init.el
@@ -4,6 +4,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/Dropbox/org/todos.org" "~/Dropbox/org/assignments.org" "~/Dropbox/org/notes.org" "~/Dropbox/org/school.org" "~/Dropbox/org/projects.org" "~/Dropbox/org/mobile.org" "~/Dropbox/org/scratch.org" "~/Dropbox/org/scratch.org" "~/Dropbox/org/prodo.org")))
  '(package-selected-packages (quote (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

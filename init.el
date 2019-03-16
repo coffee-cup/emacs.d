700aca3f8f20c3884bee55cd58ced4c2e83fb2cd
Jake Runzer <jakerunzer@gmail.com>
2019-03-16 16:46:23 +0000

    new emacs config

--- init.el
+++ init.el
@@ -1,2 +1,16 @@
 (org-babel-load-file "~/.emacs.d/config.org")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (dash-at-point all-the-icons-dired all-the-icons-dired-mode dired-x dired-details dired-dired+ dired+ drag-stuff move-text ace-window emojify doom-modeline atom-one-dark-theme all-the-icons evil-surround use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

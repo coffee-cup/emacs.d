9dd41035f43fe55ec203bdb6205688979dcf4d0b
Jake Runzer <jakerunzer@gmail.com>
2019-03-16 14:24:15 +0000

    config reset

--- init.el
+++ init.el
@@ -1,18 +1,2 @@
 (org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/Dropbox/org/todos.org" "~/Dropbox/org/assignments.org" "~/Dropbox/org/notes.org" "~/Dropbox/org/mobile.org" "~/Dropbox/org/prodo.org")))
- '(package-selected-packages (quote (use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'narrow-to-region 'disabled nil)
-(put 'narrow-to-page 'disabled nil)
+

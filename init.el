c29756229015c8eaef0f9db2c544f23860f33159
Jake Runzer <jakerunzer@gmail.com>
2019-07-12 12:25:04 +0100

    highlight at point and golden ratio

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ace-popup-menu frog-jump-buffer zenburn-theme zenburn smartparens-mode smartparens evil-escape cargo indium paradox sublimity gist gh logito pcache rust-mode yasnippet-snippets treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs dash-at-point all-the-icons-dired all-the-icons-dired-mode dired-x dired-details dired-dired+ dired+ drag-stuff move-text ace-window emojify doom-modeline atom-one-dark-theme all-the-icons evil-surround use-package)))
+    (golden-ratio ace-popup-menu frog-jump-buffer zenburn-theme zenburn smartparens-mode smartparens evil-escape cargo indium paradox sublimity gist gh logito pcache rust-mode yasnippet-snippets treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs dash-at-point all-the-icons-dired all-the-icons-dired-mode dired-x dired-details dired-dired+ dired+ drag-stuff move-text ace-window emojify doom-modeline atom-one-dark-theme all-the-icons evil-surround use-package)))
  '(treemacs-collapse-dirs 3)
  '(treemacs-filewatch-mode t)
  '(treemacs-follow-after-init t)
@@ -18,11 +18,13 @@
  '(treemacs-width 40)
  '(wakatime-cli-path "/usr/local/bin/wakatime")
  '(wakatime-python-bin nil))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(hi-yellow ((t (:background "#1b4139522e14" :foreground "#a4a3959450d0"))))
  '(magit-diff-added ((t (:background "#1d8401" :foreground "#ddffdd"))))
  '(treemacs-git-added-face ((t (:inherit font-lock-type-face :foreground "light green"))))
  '(treemacs-git-modified-face ((t (:inherit font-lock-variable-name-face :foreground "gold")))))
